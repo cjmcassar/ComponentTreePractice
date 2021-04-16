eb0127b0e40351a54130fdbbbc5d7a037cab098e
Christopher-John Cassar <46441540+cjmcassar@users.noreply.github.com>
2021-04-16 19:57:12 +0100

    Placed in a deleteItem function and edited the ToDoItem components

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,19 +1,13 @@
 import React, { useState } from "react";
 
 function ToDoItem(props) {
-  const [isDone, setIsDone] = useState(false);
-
-  function handleClick() {
-    setIsDone((prevValue) => {
-      return !prevValue;
-    });
-  }
-
   return (
-    <div onClick={handleClick}>
-      <li style={{ textDecoration: isDone ? "Line-through" : "none" }}>
-        {props.text}
-      </li>
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
     </div>
   );
 }
